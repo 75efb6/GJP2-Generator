93a2f09cf49cd563664f1d47c98b343c8cf0d6ba
75efb6 <71222525+75efb6@users.noreply.github.com>
2025-08-18 16:52:24 +0200

    Adding GJP1 generation as well.

--- gjp2_generator.py
+++ gjp2_generator.py
@@ -6,13 +6,21 @@ class PasswordHasher:
   def __init__(self, password):
     self.password = password
   
+  @property
   def gen_sha1(self):
     salt = "mI29fmAnxgTs" # Hardcoded salt
-    self.password = (self.password + salt).encode()
+    self.password = (self.password + salt).encode("utf-8")
     return hashlib.sha1(self.password).hexdigest()
   
+  @property
   def gen_gjp2(self):
-    bpasswd = self.gen_sha1().encode("utf-8")
+    bpasswd = self.gen_sha1.encode("utf-8")
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(bpasswd, salt).decode("utf-8")
+  
+  @property
+  def gen_gjp1(self):
+    bpasswd = self.password.encode("utf-8")
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(bpasswd, salt).decode("utf-8")
     
@@ -23,7 +31,7 @@ def main():
   passwd = args[1]
   print("Hashing...")
   hasher = PasswordHasher(passwd)
-  print(f"Generated hash: {hasher.gen_gjp2()}")
+  print(f"Generated GJP1 hash (password field): {hasher.gen_gjp1}\nGenerated GJP2 hash (gjp2 field): {hasher.gen_gjp2}")
 
 if __name__ == "__main__":
     main()
