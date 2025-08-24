71a6262418afe563acafe80b210281f62f2de4c0
75efb6 <71222525+75efb6@users.noreply.github.com>
2025-08-24 13:19:25 +0200

    Adding length checks

--- gjp2_generator.py
+++ gjp2_generator.py
@@ -42,8 +42,10 @@ def main():
   if not len(args) == 2:
     try:
       pw_lenght = int(input("No password inputted or inputs surpass one\nProceeding with generating a password, input what lenght you would like it to have: "))
-    except:
-      print("No lenght specified or lenght is not valid, proceeding with 16 characters.")
+      if pw_lenght < 8:
+        raise ValueError("Lenght too short")
+    except ValueError as e:
+      print(f"No lenght specified or lenght is not valid ({e}), proceeding with 16 characters.")
       pw_lenght = 16
     
     generator = PasswordGenerator(pw_lenght)
@@ -52,6 +54,8 @@ def main():
     print(f"Generated password is: {passwd}")
   else:
     passwd = args[1]
+    if lenght(passwd) < 8:
+      raise ValueError("Password is too short (min 8 chars)")
   print("Hashing...")
   hasher = PasswordHasher(passwd)
   print(f"Generated GJP1 hash (password field): {hasher.gen_gjp1}\nGenerated GJP2 hash (gjp2 field): {hasher.gen_gjp2}")
